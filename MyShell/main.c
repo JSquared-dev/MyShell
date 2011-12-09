6882264db2cd95ec285575d875e59ce6b670aa53
xddunce <xddunce@googlemail.com>
2011-12-09 19:28:58 +0000

    Initial Commit

---
+++ MyShell/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MyShell
+//
+//  Created by James Johns on 09/12/2011.
+//  Copyright 2011 xD-Duncesoft. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
